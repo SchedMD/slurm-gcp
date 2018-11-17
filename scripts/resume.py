af02ff4ae4c81095ff5df5882ae5830be5f6f419
Brian Christiansen <brian@schedmd.com>
2018-11-16 22:11:39 -0700

    Reindent code to bring closer to PEP 8
    Was ts=2. Changed to ts=4.
    Change from using % to .format()

--- scripts/resume.py
+++ scripts/resume.py
@@ -43,7 +43,7 @@ DISK_TYPE    = '@DISK_TYPE@'
 LABELS       = '@LABELS@'
 
 NETWORK_TYPE = 'subnetwork'
-NETWORK      = "projects/%s/regions/%s/subnetworks/%s-slurm-subnet" % (PROJECT, REGION, CLUSTER_NAME)
+NETWORK      = "projects/{}/regions/{}/subnetworks/{}-slurm-subnet".format(PROJECT, REGION, CLUSTER_NAME)
 
 GPU_TYPE     = '@GPU_TYPE@'
 GPU_COUNT    = '@GPU_COUNT@'
@@ -53,104 +53,109 @@ LOGFILE      = '/apps/slurm/log/resume.log'
 
 # [START create_instance]
 def create_instance(compute, project, zone, instance_type, instance_name):
-  # Get the latest CentOS 7image.
-  image_response = compute.images().getFromFamily(
-    project='centos-cloud', family='centos-7').execute()
-  source_disk_image = image_response['selfLink']
+    # Get the latest CentOS 7image.
+    image_response = compute.images().getFromFamily(
+        project='centos-cloud', family='centos-7').execute()
+    source_disk_image = image_response['selfLink']
 
-  # Configure the machine
-  machine_type = "zones/%s/machineTypes/%s" % (zone, instance_type)
-  disk_type = "projects/%s/zones/%s/diskTypes/%s" % (PROJECT, ZONE, DISK_TYPE)
-  startup_script = open('/apps/slurm/scripts/startup-script.py', 'r').read()
+    # Configure the machine
+    machine_type = "zones/{}/machineTypes/{}".format(zone, instance_type)
+    disk_type = "projects/{}/zones/{}/diskTypes/{}".format(PROJECT, ZONE,
+                                                           DISK_TYPE)
+    startup_script = open('/apps/slurm/scripts/startup-script.py', 'r').read()
 
-  config = {
-    'name': instance_name,
-    'machineType': machine_type,
+    config = {
+        'name': instance_name,
+        'machineType': machine_type,
 
-    # Specify the boot disk and the image to use as a source.
-    'disks': [
-      {
-        'boot': True,
-        'autoDelete': True,
-        'initializeParams': {
-          'sourceImage': source_disk_image,
-          'diskType': disk_type,
-          'diskSizeGb': DISK_SIZE_GB
+        # Specify the boot disk and the image to use as a source.
+        'disks': [{
+            'boot': True,
+            'autoDelete': True,
+            'initializeParams': {
+                'sourceImage': source_disk_image,
+                'diskType': disk_type,
+                'diskSizeGb': DISK_SIZE_GB
+            }
+        }],
+
+        # Specify a network interface
+        'networkInterfaces': [{
+            NETWORK_TYPE : NETWORK,
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/cloud-platform'
+            ]
+        }],
+
+        'tags': {'items': ['compute'] },
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script',
+                'value': startup_script
+            }, {
+                'key': 'enable-oslogin',
+                'value': 'TRUE'
+            }]
         }
-      }
-    ],
-
-    # Specify a network interface
-    'networkInterfaces': [{
-      NETWORK_TYPE : NETWORK,
-    }],
-
-    # Allow the instance to access cloud storage and logging.
-    'serviceAccounts': [{
-      'email': 'default',
-      'scopes': [
-        'https://www.googleapis.com/auth/cloud-platform'
-      ]
-    }],
-
-    'tags': {'items': ['compute'] },
-
-    # Metadata is readable from the instance and allows you to
-    # pass configuration from deployment scripts to instances.
-    'metadata': {
-      'items': [{
-        # Startup script is automatically executed by the
-        # instance upon startup.
-        'key': 'startup-script',
-        'value': startup_script
-      }, {
-        'key': 'enable-oslogin',
-        'value': 'TRUE'
-      }]
     }
-  }
 
-  if GPU_TYPE:
-    config['guestAccelerators'] = [{
-        'acceleratorCount': GPU_COUNT,
-        'acceleratorType' : 'https://www.googleapis.com/compute/v1/projects/' + PROJECT + '/zones/' + ZONE + '/acceleratorTypes/' + GPU_TYPE
+    if GPU_TYPE:
+        accel_type = ("https://www.googleapis.com/compute/v1/"
+                      "projects/{}/zones/{}/acceleratorTypes/{}".format(
+                          PROJECT, ZONE, GPU_TYPE))
+        config['guestAccelerators'] = [{
+            'acceleratorCount': GPU_COUNT,
+            'acceleratorType' : accel_type
         }]
 
-    config['scheduling'] = {'onHostMaintenance': 'TERMINATE'}
+        config['scheduling'] = {'onHostMaintenance': 'TERMINATE'}
 
-  if PREEMPTIBLE:
-      config['scheduling'] = {
-              "preemptible": True,
-              "onHostMaintenance": "TERMINATE",
-              "automaticRestart": False
-              },
+    if PREEMPTIBLE:
+        config['scheduling'] = {
+            "preemptible": True,
+            "onHostMaintenance": "TERMINATE",
+            "automaticRestart": False
+        },
 
-  if LABELS:
-      config['labels'] = {instance_name: LABELS},
+    if LABELS:
+        config['labels'] = {instance_name: LABELS},
 
-  if CPU_PLATFORM:
-      config['minCpuPlatform'] = CPU_PLATFORM,
+    if CPU_PLATFORM:
+        config['minCpuPlatform'] = CPU_PLATFORM,
 
-  if VPC_SUBNET:
-      config['networkInterfaces'] = [{
-      	NETWORK_TYPE : "projects/%s/regions/%s/subnetworks/%s" % (PROJECT, REGION, VPC_SUBNET)
-      }]
+    if VPC_SUBNET:
+        net_type = "projects/{}/regions/{}/subnetworks/{}".format(
+            PROJECT, REGION, VPC_SUBNET)
+        config['networkInterfaces'] = [{
+            NETWORK_TYPE : net_type
+        }]
 
-  if SHARED_VPC_HOST_PROJ:
-      config['networkInterfaces'] = [{
-      	NETWORK_TYPE : "projects/%s/regions/%s/subnetworks/%s" % (SHARED_VPC_HOST_PROJ, REGION, VPC_SUBNET)
-      }]
+    if SHARED_VPC_HOST_PROJ:
+        net_type = "projects/{}/regions/{}/subnetworks/{}".format(
+            SHARED_VPC_HOST_PROJ, REGION, VPC_SUBNET)
+        config['networkInterfaces'] = [{
+            NETWORK_TYPE : net_type
+        }]
 
-  if EXTERNAL_IP or SHARED_VPC_HOST_PROJ:
-      config['networkInterfaces'][0]['accessConfigs'] = [
-                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-             ]
+    if EXTERNAL_IP or SHARED_VPC_HOST_PROJ:
+        config['networkInterfaces'][0]['accessConfigs'] = [
+            {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+        ]
 
-
-  return compute.instances().insert(
-    project=project,
-    zone=zone,
-    body=config).execute()
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
 # [END create_instance]
 
 # [START wait_for_operation]
@@ -173,69 +178,72 @@ def wait_for_operation(compute, project, zone, operation):
 
 # [START main]
 def main(short_node_list):
-  logging.info("Bursting out:" + short_node_list)
-  compute = googleapiclient.discovery.build('compute', 'v1',
-                                            cache_discovery=False)
+    logging.info("Bursting out:" + short_node_list)
+    compute = googleapiclient.discovery.build('compute', 'v1',
+                                              cache_discovery=False)
 
-  # Get node list
-  show_hostname_cmd = "%s show hostname %s" % (SCONTROL, short_node_list)
-  node_list = subprocess.check_output(shlex.split(show_hostname_cmd))
+    # Get node list
+    show_hostname_cmd = "{} show hostname {}".format(SCONTROL, short_node_list)
+    node_list = subprocess.check_output(shlex.split(show_hostname_cmd))
 
-  operations = {}
-  for node_name in node_list.splitlines():
-    try:
-      instance = compute.instances().get(
+    operations = {}
+    for node_name in node_list.splitlines():
+        try:
+            instance = compute.instances().get(
+                      project=PROJECT, zone=ZONE, instance=node_name,
+                      fields='name,status').execute()
+            logging.info("node {} already exists in state {}".format(
+                node_name, instance['status']))
+            operations[node_name] = compute.instances().start(
+                project=PROJECT, zone=ZONE, instance=node_name).execute()
+            logging.info("Sent start instance for " + node_name)
+        except:
+            try:
+                operations[node_name] = create_instance(
+                    compute, PROJECT, ZONE, MACHINE_TYPE, node_name)
+                logging.info("Sent create instance for " + node_name)
+            except Exception, e:
+                logging.exception("Error in creation of {} ({})".format(
+                    node_name, str(e)))
+                cmd = "{} update node={} state=down reason='{}'".format(
+                    SCONTROL, node_name, str(e))
+                subprocess.call(shlex.split(cmd))
+
+    for node_name in operations:
+        try:
+            operation = operations[node_name]
+            # Do this after the instances have been initialized and then wait
+            # for all operations to finish. Then updates their addrs.
+            wait_for_operation(compute, PROJECT, ZONE, operation['name'])
+
+            my_fields = 'networkInterfaces(name,network,networkIP,subnetwork)'
+            instance_networks = compute.instances().get(
                 project=PROJECT, zone=ZONE, instance=node_name,
-                fields='name,status').execute()
-      logging.info("node %s already exists in state %s" % (node_name,
-          instance['status']))
-      operations[node_name] = compute.instances().start(project=PROJECT,
-              zone=ZONE, instance=node_name).execute()
-      logging.info("Sent start instance for " + node_name)
-    except:
-      try:
-        operations[node_name] = create_instance(compute, PROJECT, ZONE,
-                                                MACHINE_TYPE, node_name)
-        logging.info("Sent create instance for " + node_name)
-      except Exception, e:
-        logging.exception("Error in creation of %s (%s)" % (node_name, str(e)))
-        cmd = "%s update node=%s state=down reason='%s'" % \
-              (SCONTROL, node_name, str(e))
-        subprocess.call(shlex.split(cmd))
+                fields=my_fields).execute()
+            instance_ip = instance_networks['networkInterfaces'][0]['networkIP']
 
-  for node_name in operations:
-    try:
-      operation = operations[node_name]
-      # Do this after the instances have been initialized and then wait for all
-      # operations to finish. Then updates their addrs.
-      wait_for_operation(compute, PROJECT, ZONE, operation['name'])
+            node_update_cmd = "{} update node={} nodeaddr={}".format(
+                SCONTROL, node_name, instance_ip)
+            subprocess.call(shlex.split(node_update_cmd))
 
-      instance_networks = compute.instances().get(
-                project=PROJECT, zone=ZONE, instance=node_name,
-                fields='networkInterfaces(name,network,networkIP,subnetwork)'
-                ).execute()
-      instance_ip = instance_networks['networkInterfaces'][0]['networkIP']
-
-      node_update_cmd = "%s update node=%s nodeaddr=%s" % \
-              (SCONTROL, node_name, instance_ip)
-      subprocess.call(shlex.split(node_update_cmd))
-
-      logging.info("Instance " + node_name + " is now up")
-    except Exception, e:
-      logging.exception("Error in adding %s to slurm (%s)" % (node_name, str(e)))
+            logging.info("Instance " + node_name + " is now up")
+        except Exception, e:
+            logging.exception("Error in adding {} to slurm ({})".format(
+                node_name, str(e)))
 
 # [END main]
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument('nodes', help='Nodes to burst')
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('nodes', help='Nodes to burst')
 
-  args = parser.parse_args()
-  logging.basicConfig(filename=LOGFILE,
-                      format='%(asctime)s %(name)s %(levelname)s: %(message)s',
-                      level=logging.DEBUG)
+    args = parser.parse_args()
+    logging.basicConfig(
+        filename=LOGFILE,
+        format='%(asctime)s %(name)s %(levelname)s: %(message)s',
+        level=logging.DEBUG)
 
-  main(args.nodes)
+    main(args.nodes)
