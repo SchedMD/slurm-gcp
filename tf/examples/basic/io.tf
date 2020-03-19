43cd82354af430b1ade7c14710834691e4206e9f
Brian Christiansen <brian@schedmd.com>
2020-03-19 15:37:56 -0600

    Fix additional spellings
    Continutation of b0f0071a4

--- tf/examples/basic/io.tf
+++ tf/examples/basic/io.tf
@@ -202,7 +202,7 @@ variable "network_storage" {
   type = list(object({
     server_ip     = string,
     remote_mount  = string,
-    local_mout    = string,
+    local_mount   = string,
     fs_type       = string,
     mount_options = string}))
   default = []
@@ -229,7 +229,7 @@ variable "partitions" {
     network_storage = list(object({
       server_ip     = string,
       remote_mount  = string,
-      local_mout    = string,
+      local_mount   = string,
       fs_type       = string,
       mount_options = string})),
     preemptible_bursting = bool,
