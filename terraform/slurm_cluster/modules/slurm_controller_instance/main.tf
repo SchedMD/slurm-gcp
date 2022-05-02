ade29d3d9eba741ec89bd68b9042df35566b1106
Skyler Malinowski <malinowski@schedmd.com>
2022-05-02 14:48:23 -0400

    Fix config delta causing login node bounce
    The addition of login_startup_scripts mysteriously made terraform start
    bouncing login nodes whenever controller metadata changed. I believe
    this behavior is correct; the lack of behavior before seemed to be a
    bug (unconfirmed).
    
    The only real solution is to remove the dependency between the
    controller and project metadata containing configuration data. This way
    the config can change but the controller will only delta on changes to
    the hardware configuration, not the slurm configuration.

--- terraform/slurm_cluster/modules/slurm_controller_instance/main.tf
+++ terraform/slurm_cluster/modules/slurm_controller_instance/main.tf
@@ -148,16 +148,7 @@ module "slurm_controller_instance" {
   subnetwork          = var.subnetwork
   zone                = var.zone
 
-  metadata = merge(
-    var.metadata,
-    {
-      slurm_depends_on_config        = sha256(google_compute_project_metadata_item.config.value)
-      slurm_depends_on_cgroup_conf   = sha256(google_compute_project_metadata_item.cgroup_conf.value)
-      slurm_depends_on_slurm_conf    = sha256(google_compute_project_metadata_item.slurm_conf.value)
-      slurm_depends_on_slurmdbd_conf = sha256(google_compute_project_metadata_item.slurmdbd_conf.value)
-      slurm_depends_on_devel         = var.enable_devel ? sha256(module.slurm_metadata_devel[0].metadata.value) : null
-    },
-  )
+  metadata = var.metadata
 
   depends_on = [
     google_compute_project_metadata_item.controller_startup_scripts,
@@ -418,7 +409,7 @@ module "reconfigure_notify" {
     # Ensure subscriptions are created
     module.slurm_pubsub,
     # Ensure controller is created
-    module.slurm_controller_instance,
+    module.slurm_controller_instance.slurm_instances,
   ]
 }
 
