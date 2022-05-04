SchedMD/slurm-gcp

commit ade29d3d9eba741ec89bd68b9042df35566b1106

Fix config delta causing login node bounce
The addition of login_startup_scripts mysteriously made terraform start
bouncing login nodes whenever controller metadata changed. I believe
this behavior is correct; the lack of behavior before seemed to be a
bug (unconfirmed).

The only real solution is to remove the dependency between the
controller and project metadata containing configuration data. This way
the config can change but the controller will only delta on changes to
the hardware configuration, not the slurm configuration.

terraform/slurm_cluster/modules/slurm_controller_instance/main.tf
@@ -148,16 +148,7 @@ module "slurm_controller_instance" {
   subnetwork          = var.subnetwork
   zone                = var.zone
 
-  metadata = merge(
-    var.metadata,
-    {
-      slurm_depends_on_config        = sha256(google_compute_project_metadata_item.config.value)
-      slurm_depends_on_cgroup_conf   = sha256(google_compute_project_metadata_item.cgroup_conf.value)
-      slurm_depends_on_slurm_conf    = sha256(google_compute_project_metadata_item.slurm_conf.value)
-      slurm_depends_on_slurmdbd_conf = sha256(google_compute_project_metadata_item.slurmdbd_conf.value)
-      slurm_depends_on_devel         = var.enable_devel ? sha256(module.slurm_metadata_devel[0].metadata.value) : null
-    },
-  )
+  metadata = var.metadata
 
   depends_on = [
     google_compute_project_metadata_item.controller_startup_scripts,
@@ -418,7 +409,7 @@ module "reconfigure_notify" {
     # Ensure subscriptions are created
     module.slurm_pubsub,
     # Ensure controller is created
-    module.slurm_controller_instance,
+    module.slurm_controller_instance.slurm_instances,
   ]
 }
 

commit 681425fc17db86f4208e94fa9b50b47f47d83d01

Fix project metadata created too long after controller
Regression caused by ade29d3d9eba741ec89bd68b9042df35566b1106

terraform/slurm_cluster/main.tf
@@ -205,7 +205,7 @@ module "slurm_login_template" {
 }
 
 ###################
-# LOGIN: TEMPLATE #
+# LOGIN: INSTANCE #
 ###################
 
 module "slurm_login_instance" {
@@ -228,8 +228,8 @@ module "slurm_login_instance" {
   subnetwork            = each.value.subnetwork
   zone                  = each.value.zone
 
-  depends_on = [
+  slurm_depends_on = flatten([
     # Ensure Controller is up before attempting to mount file systems from it
-    module.slurm_controller_instance[0].slurm_controller_instance,
-  ]
+    module.slurm_controller_instance[0].slurm_controller_instances[*].instance_id,
+  ])
 }

terraform/slurm_cluster/modules/_slurm_instance/main.tf
@@ -101,4 +101,8 @@ resource "google_compute_instance_from_template" "slurm_instance" {
       VmDnsSetting        = "GlobalOnly"
     },
   )
+
+  depends_on = [
+    var.slurm_depends_on,
+  ]
 }

terraform/slurm_cluster/modules/_slurm_instance/outputs.tf
@@ -15,6 +15,11 @@
  * limitations under the License.
  */
 
+output "slurm_instances" {
+  description = "List of all resource objects for compute instances"
+  value       = google_compute_instance_from_template.slurm_instance
+}
+
 output "instances_self_links" {
   description = "List of self-links for compute instances"
   value       = google_compute_instance_from_template.slurm_instance.*.self_link

terraform/slurm_cluster/modules/_slurm_instance/variables.tf
@@ -120,3 +120,14 @@ variable "slurm_cluster_name" {
   description = "Cluster name, used for resource naming."
   type        = string
 }
+
+variable "slurm_depends_on" {
+  description = <<EOD
+Custom terraform dependencies without replacement on delta. This is useful to
+ensure order of resource creation.
+
+NOTE: Also see terraform meta-argument 'depends_on'.
+EOD
+  type        = list(string)
+  default     = []
+}

terraform/slurm_cluster/modules/slurm_controller_instance/main.tf
@@ -150,6 +150,13 @@ module "slurm_controller_instance" {
 
   metadata = var.metadata
 
+  slurm_depends_on = [
+    google_compute_project_metadata_item.config.id,
+    google_compute_project_metadata_item.slurm_conf.id,
+    google_compute_project_metadata_item.cgroup_conf.id,
+    google_compute_project_metadata_item.slurmdbd_conf.id,
+    var.enable_devel ? module.slurm_metadata_devel[0].metadata.id : null,
+  ]
   depends_on = [
     google_compute_project_metadata_item.controller_startup_scripts,
     # Ensure nodes are destroyed before controller is

terraform/slurm_cluster/modules/slurm_controller_instance/outputs.tf
@@ -19,8 +19,13 @@
 ############
 
 output "slurm_controller_instance" {
-  description = "Controller instance"
+  description = "Controller instance module."
   value       = module.slurm_controller_instance
+}
+
+output "slurm_controller_instances" {
+  description = "Controller instance resource."
+  value       = module.slurm_controller_instance.slurm_instances
 }
 
 #########

terraform/slurm_cluster/modules/slurm_login_instance/main.tf
@@ -66,6 +66,7 @@ module "slurm_login_instance" {
   subnetwork          = var.subnetwork
   zone                = var.zone
 
+  slurm_depends_on = var.slurm_depends_on
   depends_on = [
     # Ensure delta when user startup scripts change
     google_compute_project_metadata_item.login_startup_scripts,

terraform/slurm_cluster/modules/slurm_login_instance/variables.tf
@@ -115,3 +115,14 @@ variable "slurm_cluster_name" {
     error_message = "Variable 'slurm_cluster_name' must be a match of regex '(^[a-z][a-z0-9]*$)'."
   }
 }
+
+variable "slurm_depends_on" {
+  description = <<EOD
+Custom terraform dependencies without replacement on delta. This is useful to
+ensure order of resource creation.
+
+NOTE: Also see terraform meta-argument 'depends_on'.
+EOD
+  type        = list(string)
+  default     = []
+}

terraform/slurm_cluster/outputs.tf
@@ -28,6 +28,15 @@ output "slurm_partition" {
   )
 }
 
+output "slurm_controller_instances" {
+  description = "Slurm controller instance object details."
+  value = (
+    var.enable_hybrid
+    ? []
+    : module.slurm_controller_instance[0].slurm_controller_instances
+  )
+}
+
 output "slurm_controller_instance_self_links" {
   description = "Slurm controller instance self_link."
   value = (