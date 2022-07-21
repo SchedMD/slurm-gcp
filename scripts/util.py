SchedMD/slurm-gcp

commit 460a96742168cae3fede0fde06122864f619925e

Allow configuring controller hostname for hybrid deployments
Issue #243

scripts/util.py
@@ -336,6 +336,8 @@ def load_config_data(config):
         cfg.slurm_log_dir = dirs.log
     if not cfg.slurm_bin_dir:
         cfg.slurm_bin_dir = slurmdirs.prefix / "bin"
+    if not cfg.slurm_control_host:
+        cfg.slurm_control_host = f"{cfg.slurm_cluster_name}-controller"
     return cfg
 
 
@@ -354,8 +356,8 @@ def new_config(config):
         ),
     )
     for netstore in network_storage_iter:
-        if netstore.server_ip == "$controller":
-            netstore.server_ip = cfg.slurm_cluster_name + "-controller"
+        if not netstore.server_ip or netstore.server_ip == "$controller":
+            netstore.server_ip = cfg.slurm_control_host
     return cfg
 
 

terraform/slurm_cluster/examples/slurm_cluster/hybrid/basic/example.tfvars
@@ -83,6 +83,7 @@ epilog_scripts = [
 
 controller_hybrid_config = {
   google_app_cred_path = null
+  slurm_control_host   = null
   slurm_bin_dir        = "/usr/local/bin"
   slurm_log_dir        = "./log"
   output_dir           = "./etc"

terraform/slurm_cluster/examples/slurm_cluster/hybrid/basic/variables.tf
@@ -166,12 +166,14 @@ Variables map to:
 EOD
   type = object({
     google_app_cred_path = string
+    slurm_control_host   = string
     slurm_bin_dir        = string
     slurm_log_dir        = string
     output_dir           = string
   })
   default = {
     google_app_cred_path = null
+    slurm_control_host   = null
     slurm_bin_dir        = "/usr/local/bin"
     slurm_log_dir        = "/var/log/slurm"
     output_dir           = "/etc/slurm"

terraform/slurm_cluster/examples/slurm_cluster/hybrid/full/example.tfvars
@@ -96,6 +96,7 @@ epilog_scripts = [
 
 controller_hybrid_config = {
   google_app_cred_path = null
+  slurm_control_host   = null
   slurm_bin_dir        = "/usr/local/bin"
   slurm_log_dir        = "./log"
   output_dir           = "./etc"

terraform/slurm_cluster/examples/slurm_cluster/hybrid/full/variables.tf
@@ -217,12 +217,14 @@ Variables map to:
 EOD
   type = object({
     google_app_cred_path = string
+    slurm_control_host   = string
     slurm_bin_dir        = string
     slurm_log_dir        = string
     output_dir           = string
   })
   default = {
     google_app_cred_path = null
+    slurm_control_host   = null
     slurm_bin_dir        = "/usr/local/bin"
     slurm_log_dir        = "/var/log/slurm"
     output_dir           = "."

terraform/slurm_cluster/examples/slurm_cluster/hybrid/simple/main.tf
@@ -125,6 +125,7 @@ locals {
 
   controller_hybrid_config = {
     google_app_cred_path = null
+    slurm_control_host   = null
     slurm_log_dir        = "/var/log/slurm"
     slurm_bin_dir        = "/usr/local/bin"
     output_dir           = "./etc"

terraform/slurm_cluster/main.tf
@@ -146,6 +146,7 @@ module "slurm_controller_hybrid" {
   slurm_cluster_name = var.slurm_cluster_name
 
   google_app_cred_path         = var.controller_hybrid_config.google_app_cred_path
+  slurm_control_host           = var.controller_hybrid_config.slurm_control_host
   slurm_bin_dir                = var.controller_hybrid_config.slurm_bin_dir
   slurm_log_dir                = var.controller_hybrid_config.slurm_log_dir
   output_dir                   = var.controller_hybrid_config.output_dir

terraform/slurm_cluster/modules/slurm_controller_hybrid/main.tf
@@ -74,6 +74,7 @@ locals {
     # hybrid
     google_app_cred_path = local.google_app_cred_path
     output_dir           = local.output_dir
+    slurm_control_host   = var.slurm_control_host
     slurm_bin_dir        = local.slurm_bin_dir
     slurm_log_dir        = local.slurm_log_dir
   })

commit 499e5c749adb9ce4a27d210835c4b05a8cdc45e9

Use correct falsy
Regression from commit 460a967

Issue #243

scripts/util.py
@@ -356,7 +356,7 @@ def new_config(config):
         ),
     )
     for netstore in network_storage_iter:
-        if not netstore.server_ip or netstore.server_ip == "$controller":
+        if netstore.server_ip is None or netstore.server_ip == "$controller":
             netstore.server_ip = cfg.slurm_control_host
     return cfg
 