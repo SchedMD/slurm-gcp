SchedMD/slurm-gcp

commit 756b83e71942bbb110d8ac404d91263fe5315460

Fix unused edge case in load_bq.py
No behavior change.

scripts/load_bq.py
@@ -22,7 +22,7 @@ SACCT = "sacct"
 script = Path(__file__).resolve()
 
 DEFAULT_TIMESTAMP_FILE = script.parent / "bq_timestamp"
-timestamp_file = os.environ.get("TIMESTAMP_FILE") or DEFAULT_TIMESTAMP_FILE
+timestamp_file = Path(os.environ.get("TIMESTAMP_FILE")) or DEFAULT_TIMESTAMP_FILE
 
 # cluster_id_file = script.parent / 'cluster_uuid'
 # try:

commit 97efee4903e1624e1a071759cbb0c47a3abd28e8

Fix regression in load_bq.py
regression in commit 756b83e71942bbb110d8ac404d91263fe5315460.

scripts/load_bq.py
@@ -22,7 +22,7 @@ SACCT = "sacct"
 script = Path(__file__).resolve()
 
 DEFAULT_TIMESTAMP_FILE = script.parent / "bq_timestamp"
-timestamp_file = Path(os.environ.get("TIMESTAMP_FILE")) or DEFAULT_TIMESTAMP_FILE
+timestamp_file = Path(os.environ.get("TIMESTAMP_FILE", DEFAULT_TIMESTAMP_FILE))
 
 # cluster_id_file = script.parent / 'cluster_uuid'
 # try: