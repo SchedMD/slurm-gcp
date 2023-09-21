d6127afd9f06d56bcdf9f146ac5520dac8b3e2dc
Broderick Gardner <broderick@schedmd.com>
2023-09-21 11:45:04 -0600

    Update image references for 5.9 release

--- terraform/slurm_cluster/modules/slurm_instance_template/main.tf
+++ terraform/slurm_cluster/modules/slurm_instance_template/main.tf
@@ -51,7 +51,7 @@ locals {
   source_image_family = (
     var.source_image_family != "" && var.source_image_family != null
     ? var.source_image_family
-    : "slurm-gcp-5-8-hpc-centos-7"
+    : "slurm-gcp-5-9-hpc-centos-7"
   )
   source_image_project = (
     var.source_image_project != "" && var.source_image_project != null
